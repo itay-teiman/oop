1fbb450d207fa06e43609ac6aaac84661fe04ee8
itay-teiman <itayteiman@gmail.com>
2024-09-24 14:50:58 +0300

    inut

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-/////
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,11 +13,12 @@ public class Main {
         int num1 = myObj.nextInt();
         System.out.println("type num Two");
         int num2 = myObj.nextInt();
-
+//
         System.out.println("+"+" "+(num1+num2));
         System.out.println("-"+" "+ (num1-num2));
         System.out.println("*"+" "+(num1*num2));
         System.out.println("/"+" "+(double)(num1/num2));
+        System.out.println("itay");
 
 
 
